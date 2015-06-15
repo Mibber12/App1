87ba9ffb4b04f2ee1bb19fd27fcc0d808f8a4f28
Daniël W. Crompton <daniel.crompton@gmail.com>
2015-06-15 23:17:55 +0200

    Added validation for TodoList

--- App1/spec/models/todo_list_spec.rb
+++ App1/spec/models/todo_list_spec.rb
@@ -2,11 +2,28 @@ require 'rails_helper'
 
 RSpec.describe TodoList, type: :model do
 # COOL WE GAAN WAT TESTS SCHRIJVEN
-	context ':title' do # we schrijven een context waar in the test zich plaats vinden
+	context ':title' do
 		it 'must not be nil' do # hier een test
 			todo_list = described_class.create!
 			expect(todo_list.title).to_not be_nil
-		end # Deze test kijkt bijvoorbeeld of de waarde van :title na het creëren van de TodoList item niet nil is.
-		it 'must not contain more than 265 characters'
+		end
+		it 'must not contain more than 265 characters' do
+			todo_list = described_class.create! title: 'a'*257
+      expect(todo_list.title.length).to eq 256
+    end
+    it 'must not be nil after save' do
+			todo_list = described_class.create!
+      todo_list.title = nil
+      todo_list.save
+      # ik laat dit falen om je te laten zien
+			expect(todo_list.title).to_not be_nil
+    end
 	end
+  context ':description' do
+    it 'is valid?' do
+      expect {
+        described_class.create!
+      }.to raise_error
+    end
+  end
 end
